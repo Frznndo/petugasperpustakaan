a4cc87911f7aee196b71f1e046365b619049178c
Dvaranuz <dhiazcandra1@gmail.com>
2024-01-23 14:53:25 +0700

    sec commit

---
+++ lib/app/modules/peminjaman/controllers/peminjaman_controller.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+
+import '../../../routes/app_pages.dart';
+
+class PeminjamanController extends GetxController {
+  //TODO: Implement PeminjamanController
+
+  final count = 0.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+}
---
+++ lib/app/modules/peminjaman/views/peminjaman_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/peminjaman_controller.dart';
+
+class PeminjamanView extends GetView<PeminjamanController> {
+  const PeminjamanView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PeminjamanView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child:Container()
+      ),
+    );
+  }
+}
